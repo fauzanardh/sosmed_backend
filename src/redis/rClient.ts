116f5815b0ce381e7520e86f1fe124cbf773d85f
Fauzan Ardhana <31564867+fauzanardh@users.noreply.github.com>
2021-04-04 20:04:12 +0700

    Integrating Redis to TypeORM caching system (#5)

--- src/redis/rClient.ts
+++ src/redis/rClient.ts
@@ -1,29 +1,3 @@
 import Redis from "ioredis";
 
 export const client = new Redis(Number(process.env.REDIS_PORT), "redis");
-
-export interface RedisJobs {
-    using_cache: boolean,
-    jobs: string
-}
-
-// This function is a helper function for retrieving data from the redis cache
-// returns a promise object
-export const get = (key: string) => {
-    return new Promise((resolve, reject) => {
-        client.get(key, (err, jobs) => {
-            if (err) reject(err);
-
-            if (jobs) {
-                resolve({using_cache: true, jobs: jobs});
-            } else {
-                resolve({using_cache: false, jobs: jobs});
-            }
-        });
-    });
-}
-
-export default {
-    client,
-    get
-}
