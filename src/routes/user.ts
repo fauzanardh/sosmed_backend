70fb67589510571bac9a5c45b4374ec2665e2254
Fauzan Ardhana <31564867+fauzanardh@users.noreply.github.com>
2021-04-04 19:23:06 +0700

    Add search function for user table

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {createUser, getUserByUUID, getUsers, updateUser, deleteUser} from "../controllers/user";
+import {createUser, getUserByUUID, getUsers, updateUser, deleteUser, searchUser} from "../controllers/user";
 import jwt from "express-jwt";
 import {handleJWTError} from "../middlewares/jwt";
 
@@ -7,6 +7,7 @@ const router = Router();
 
 router.get('/', getUsers);
 router.get('/:uuid', getUserByUUID);
+router.get('/search/:searchString', searchUser)
 router.post('/', createUser);
 router.patch(
     '/',
